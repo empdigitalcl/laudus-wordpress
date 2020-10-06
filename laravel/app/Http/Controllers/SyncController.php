577e3e36c855730cb653ef9f62321d99d4f74e45
Ricardo Carrasco <ricardo@empdigital.cl>
2020-10-06 12:35:07 -0300

    Close sync stock laudus

--- laravel/app/Http/Controllers/SyncController.php
+++ laravel/app/Http/Controllers/SyncController.php
@@ -187,7 +187,7 @@ class SyncController extends Controller
             foreach ($response as $product) {
                 $sync = Sync::BySku($product->code)->first();
                 if ($sync && $sync->count() > 0) {
-                    echo $product->code.'>'.$product->stock.' !='. $sync->stockAvailable.'<br>';
+                    // echo $product->code.'>'.$product->stock.' !='. $sync->stockAvailable.'<br>';
                     if ($product->stock != $sync->stockAvailable) {
                         $isUpdate = true;
                         $sync->stockAvailable = $product->stock;
